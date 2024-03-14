41a97ccdc6d8d67d2bf7ec5918560cf4442e9674
emVisible <1619346963@qq.com>
2024-03-14 23:12:39 +0800

    llm api

---
+++ src/apis/llmApi.ts
@@ -0,0 +1,34 @@
+import { CacheEnum } from '@/enum/cacheEnum'
+import store from '@/utils/store'
+import { logout } from '@/utils/user'
+import { URLSearchParams } from 'url'
+import { http } from '../plugins/axios/index'
+
+
+export interface LLMRequestType {
+  model: string;
+  messages: Message[];
+  stream: boolean;
+  max_tokens: number;
+  temperature: number;
+  top_p: number;
+}
+
+interface Message {
+  role: string;
+  content: string;
+}
+
+/**
+ * 想LLM发送请求
+*/
+export function createCompletion(data: LLMRequestType) {
+  return http.request({
+    url: '/llm/completions',
+    method: 'post',
+    data
+  })
+}
+
+
+export default { createCompletion }
